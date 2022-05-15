a1e56b854d536ff9e1def762fb0460bb89418768
cryptofyre <cryptofyre@cryptofyre.org>
2022-05-14 22:53:28 -0500

    Initial commit

---
+++ Cider-Game-Bar/Widget1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Cider_Game_Bar
+{
+    #pragma warning disable CS8305
+    public sealed partial class Widget1 : Page
+    {
+        public Widget1()
+        {
+            this.InitializeComponent();
+
+            try
+            {
+                MainView.Source = new Uri("http://localhost:6942");
+            } catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Cider-Game-Bar
+ An open source UWP Game Bar module for use with Cider.
